d7627f42fee2819a7df0d52a3b9e77cb44e73f78
gonzalo <gonzalo.moros.ds@gmail.com>
2016-11-29 19:32:30 +0100

    Style about page & link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,21 @@ var React = require('react');
 var About = (props) => {
   return(
     <di>
-      <h3>About</h3>
-      <p>Welcome to about page!</p>
+      <h1 className="text-center">Acerca de</h1>
+      <p>Esta es una aplicación del tiempo construida con React.
+         Esto desarollandola como ejemplos de un curso de React.</p>
+       <p>Algunas de las herramientas utilizadas son:</p>
+       <ul>
+         <li>
+           <a href="https://facebook.github.io/react">React</a> - Este es el
+             framework JavaScript utilizado.
+         </li>
+         <li>
+           <a href="http://openweathermap.org">Open Weather Map</a> - Se ha
+             utilizado Open Weather Map para la busqueda del tiempo por
+              ciudades.
+         </li>
+       </ul>
     </di>
   )
 };
